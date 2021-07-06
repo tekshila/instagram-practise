cfe0f77e6e8ea6102644f0d9484e5e64473b7b9b
tekshila <67528077+tekshila@users.noreply.github.com>
2021-07-06 15:27:52 -0400

    first version with discovery service.

---
+++ src/main/java/com/tekshila/insta/InstagramPractiseApplication.java
@@ -0,0 +1,13 @@
+package com.tekshila.insta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramPractiseApplication.class, args);
+    }
+
+}
